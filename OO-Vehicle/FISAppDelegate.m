6a5525fa7e8da789c0f5111096ab8eb6336aa97b
TOMalley104 <tom@flatironschool.com>
2015-09-24 16:14:15 -0400

    update appdelegate to fix app breaking error

--- OO-Vehicle/FISAppDelegate.m
+++ OO-Vehicle/FISAppDelegate.m
@@ -15,6 +15,8 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    UIViewController *viewController = [[UIViewController alloc] init];
+    [self.window setRootViewController: viewController];
     [self.window makeKeyAndVisible];
     return YES;
 }
